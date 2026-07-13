e1bdec7724ea7af5b8a02c7e8933ac828fedfdb8
ADITYA SINGH GAHARWAR <adityasingh70717@gmail.com>
2026-07-14 03:44:07 +0530

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -2,8 +2,25 @@
 Accounts Microservice
 """
 
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# Create Flask application
 app = Flask(__name__)
 
+# Configuration
+app.config["SECRET_KEY"] = "dev_secret_key"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URI",
+    "sqlite:///accounts.db"
+)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Database object
+db = SQLAlchemy(app)
+
+# Import routes after app/db initialization
 from service import routes
