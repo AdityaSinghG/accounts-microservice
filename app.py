3528d424326e4af187e7647818e2e532b33e0eed
ADITYA SINGH GAHARWAR <adityasingh70717@gmail.com>
2026-07-14 03:43:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from service import create_app
+"""
+Application entry point
+"""
 
-app = create_app()
+from service import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
