6384e70d8c363106b4804b5879f3683b9fcf5118
ADITYA SINGH GAHARWAR <adityasingh70717@gmail.com>
2026-07-14 18:11:00 +0530

    Update models.py

--- service/models.py
+++ service/models.py
@@ -1,81 +1,63 @@
 """
 Account Model
 """
-
 from service import db
 
 
 class Account(db.Model):
-    """
-    Represents an Account
-    """
+    """Account model for storing account data"""
 
     __tablename__ = "accounts"
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
-    address = db.Column(db.String(255))
-    phone = db.Column(db.String(20))
-    disabled = db.Column(db.Boolean(), default=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+    address = db.Column(db.String(256), nullable=False)
+    phone = db.Column(db.String(32), nullable=False)
+    date_joined = db.Column(db.Date(), nullable=False, default=db.func.current_date())
 
     def __repr__(self):
-        return f"<Account {self.name}>"
-
-    def create(self):
-        """
-        Creates an Account
-        """
-        db.session.add(self)
-        db.session.commit()
-
-    def update(self):
-        """
-        Updates an Account
-        """
-        db.session.commit()
-
-    def delete(self):
-        """
-        Deletes an Account
-        """
-        db.session.delete(self)
-        db.session.commit()
+        return f"<Account {self.id} {self.name}>"
 
     def serialize(self):
-        """
-        Converts Account to dictionary
-        """
+        """Serialize account to dictionary"""
         return {
             "id": self.id,
             "name": self.name,
             "email": self.email,
             "address": self.address,
             "phone": self.phone,
-            "disabled": self.disabled,
+            "date_joined": str(self.date_joined),
         }
 
     def deserialize(self, data):
-        """
-        Loads dictionary into Account
-        """
-        self.name = data.get("name")
-        self.email = data.get("email")
-        self.address = data.get("address")
-        self.phone = data.get("phone")
-        self.disabled = data.get("disabled", False)
+        """Deserialize account from dictionary"""
+        self.name = data["name"]
+        self.email = data["email"]
+        self.address = data["address"]
+        self.phone = data["phone"]
         return self
+
+    def create(self):
+        """Create account in database"""
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """Update account in database"""
+        db.session.commit()
+
+    def delete(self):
+        """Delete account from database"""
+        db.session.delete(self)
+        db.session.commit()
 
     @classmethod
     def all(cls):
-        """
-        Returns all accounts
-        """
+        """Return all accounts"""
         return cls.query.all()
 
     @classmethod
     def find(cls, account_id):
-        """
-        Finds account by id
-        """
+        """Find account by id"""
         return cls.query.get(account_id)
